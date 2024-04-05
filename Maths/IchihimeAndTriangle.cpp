4796e8c5b5e71b19656b2111832f55e7e4c0d7cc
Aniket Kumar <55664517+aniketsingh999@users.noreply.github.com>
2024-04-06 00:23:07 +0530

    Ichihime and Triangle

---
+++ Maths/IchihimeAndTriangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    long long a, b, c, d;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c>>d;
+        int x, y, z;
+        x=b;
+        y=c;
+        z=c;
+        cout<<x<<" "<<y<<" "<<z<<endl;
+    }
+    return 0;
+}
